9f7a7603c765ee05ae9574ebd9daccdbdf959245
connorbrickt <219157787+connorbrickt@users.noreply.github.com>
2025-07-14 20:14:59 -0400

    add my solutions for 09, 10, and 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let low = num1;
+    let high = num2;
+    if (num1 > num2) {
+        high = num1;
+        low = num2;
+    }
+    if (low < 0 
+        || typeof low !== "number" 
+        || typeof high !== "number" 
+        || low !== Math.floor(low) 
+        || high !== Math.floor(high)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    for (i = low; i <= high; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
